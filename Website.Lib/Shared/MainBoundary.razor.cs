c355f4a4fee5c58b5171490349d8e158b433312f
Simon Ziegler <simon@dioptra.tech>
2022-05-12 17:38:15 +0100

    Further layout upgrade

--- Website.Lib/Shared/MainBoundary.razor.cs
+++ Website.Lib/Shared/MainBoundary.razor.cs
@@ -2,7 +2,4 @@
 
 namespace Website.Lib.Shared;
 
-public partial class MainBoundary : ComponentBase
-{
-    [Parameter] public string ColorClass { get; set; }
-}
+public partial class MainBoundary : ComponentBase { }
