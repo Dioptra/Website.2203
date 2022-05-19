f5e8ddff37ea985122c414a866beaa9762c8f63b
Simon Ziegler <simon@dioptra.tech>
2022-05-19 19:27:37 +0100

    Compilation bug fixes

--- Website.BlazorWebAssembly/Client/Program.cs
+++ Website.BlazorWebAssembly/Client/Program.cs
@@ -1,7 +1,11 @@
+using Material.Blazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Website.Lib;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMBServices(loggingServiceConfiguration: Utilities.GetDefaultLoggingServiceConfiguration(), toastServiceConfiguration: Utilities.GetDefaultToastServiceConfiguration(), snackbarServiceConfiguration: Utilities.GetDefaultSnackbarServiceConfiguration());
 
 await builder.Build().RunAsync();
