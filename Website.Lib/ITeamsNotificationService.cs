b117ae251aecf5f10c5c0df36c3cdca74db7d351
Simon Ziegler <simon@dioptra.tech>
2022-05-20 15:58:15 +0100

    Basic prerendered WASM web app works

--- Website.Lib/ITeamsNotificationService.cs
+++ Website.Lib/ITeamsNotificationService.cs
@@ -4,5 +4,8 @@ using System.Threading.Tasks;
 
 public interface ITeamsNotificationService
 {
-    Task SendNotification(IMessage message);
+    Task SendNotification(ContactMessage message);
+    Task SendNotification(RecruitmentEnquiry message);
+    Task SendNotification(RealEstateInvestorEnquiry message);
+    Task SendNotification(VentureCapitalEnquiry message);
 }
