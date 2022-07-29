2b681c4e40bd510018bfc35fe18c957d14e3d813
Simon Ziegler <simon@dioptra.tech>
2022-07-29 11:10:49 +0100

    next

--- Website.Lib/PackageInformation.cs
+++ Website.Lib/PackageInformation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Linq;
 
 namespace Website.Lib;
 
@@ -6,18 +7,46 @@ namespace Website.Lib;
 /// <summary>
 /// Supplies information about the assembly/package. 
 /// </summary>
-public class PackageInformation
+public static class PackageInformation
 {
+    static PackageInformation()
+    {
+        Version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
+        BuildTime = DateTime.UtcNow;
+
+        var bt = Environment.GetEnvironmentVariable("BUILD_TIME") ?? "";
+
+        var parts = bt.Split("-");
+
+        if (parts.Length == 7)
+        {
+            parts[3] = "0";
+
+            var intParts = parts.Select(x => Convert.ToInt32(x)).ToArray();
+
+            BuildTime = new(intParts[0], intParts[1], intParts[2], intParts[4], intParts[5], intParts[6]);
+        }
+
+        BuildTimeString = BuildTime.ToShortDateString() + " " + BuildTime.ToShortTimeString() + " UTC";
+    }
+
     /// <summary>
     /// Returns a string with the value of the InformationalVersion
     /// </summary>
     /// <returns></returns>
-    public static string Version => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
-    
-    
+    public static readonly string Version;
+
+
     /// <summary>
-    /// Returns a string with the value of the InformationalVersion
+    /// The build time
     /// </summary>
     /// <returns></returns>
-    public static string Blub => Assembly.GetExecutingAssembly().GetCustomAttribute()?.InformationalVersion ?? "";
+    public static readonly DateTime BuildTime;
+
+
+    /// <summary>
+    /// Teh build time as a string
+    /// </summary>
+    /// <returns></returns>
+    public static readonly string BuildTimeString;
 }
