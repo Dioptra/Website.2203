268392ad52dc7c0dddf366271ac8ff75b714c461
Simon Ziegler <simon@dioptra.tech>
2022-07-29 09:17:01 +0100

    next

---
+++ Website.Lib/PackageInformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace Website.Lib;
+
+
+/// <summary>
+/// Supplies information about the assembly/package. 
+/// </summary>
+public class PackageInformation
+{
+    /// <summary>
+    /// Returns a string with the value of the InformationalVersion
+    /// </summary>
+    /// <returns></returns>
+    public static string Version => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
+    
+    
+    /// <summary>
+    /// Returns a string with the value of the InformationalVersion
+    /// </summary>
+    /// <returns></returns>
+    public static string Blub => Assembly.GetExecutingAssembly().GetCustomAttribute()?.InformationalVersion ?? "";
+}
